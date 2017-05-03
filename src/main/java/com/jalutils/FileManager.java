8b80cfac0e08de1682eb3d602db9b28c5c6d6747
Jose Andrés Lara <jose.andreas.lara@one.verizon.com>
2017-05-03 15:29:57 -0700

    Adding javadoc generator.

--- src/main/java/com/jalutils/FileManager.java
+++ src/main/java/com/jalutils/FileManager.java
@@ -1,15 +1,17 @@
 package com.jalutils;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 
+/**
+ * Handles file system operations. This class is implemented to be used as a
+ * Singleton Pattern.
+ * 
+ * @author larajo7
+ */
 public final class FileManager {
 
   private static FileManager instance;
@@ -18,76 +20,18 @@ public final class FileManager {
     // hide constructor
   }
 
-  public static FileManager getInstance() {
-    if (instance == null) {
-      instance = new FileManager();
-    }
-
-    return instance;
-  }
-
-  public void deleteFolder(final String folderAbsolutePath) throws IOException {
-    if (!StringUtils.getInstance().isValid(folderAbsolutePath)) {
-      throw new IllegalArgumentException("Invalid folder path");
-    }
-
-    File folder2Delete = new File(folderAbsolutePath);
-    deleteFolder(folder2Delete);
-  }
-
-  public void deleteFolder(final File folder2Delete) throws IOException {
-    if (folder2Delete == null) {
-      throw new IOException("Folder to delete can not be null.");
-    }
-
-    if (!folder2Delete.exists()) {
-      throw new IOException("Folder to delete does not exist.");
-    }
-
-    if (folder2Delete.isFile()) {
-      throw new IOException(folder2Delete.getAbsolutePath() + " is a File.");
-    }
-
-    if (folder2Delete.listFiles().length == 0) {
-      folder2Delete.delete();
-    } else {
-      for (File file : folder2Delete.listFiles()) {
-        if (file.isDirectory()) {
-          deleteFolder(file);
-        } else if (file.isFile()) {
-          deleteFile(file);
-        }
-      }
-
-      folder2Delete.delete();
-    }
-  }
-
-  public void deleteFile(final String fileAbsolutePath) throws IOException {
-    if (!StringUtils.getInstance().isValid(fileAbsolutePath)) {
-      throw new IllegalArgumentException("Invalid file path");
-    }
-
-    File file2delete = new File(fileAbsolutePath);
-    deleteFile(file2delete);
-  }
-
-  public void deleteFile(final File file2delete) throws IOException {
-    if (file2delete == null) {
-      throw new IOException("File to delete can not be null.");
-    }
-
-    if (!file2delete.exists()) {
-      throw new IOException("File to delete does not exist.");
-    }
-
-    if (file2delete.isDirectory()) {
-      throw new IOException(file2delete.getAbsolutePath() + " is a Folder.");
-    }
-
-    file2delete.delete();
-  }
-
+  /**
+   * Appends the specified extension to the file name.
+   * <p>
+   * If the file's name does not end with the the extension specified by param
+   * <code>extension</code>, then the extension is appended.
+   * 
+   * @param fileName
+   *          the current file name.
+   * @param extension
+   *          the extension to append.
+   * @return the fixed file's name ending with the extension specification.
+   */
   public String addExtension(final String fileName, final String extension) {
     if (!StringUtils.getInstance().isValid(fileName)) {
       throw new IllegalArgumentException("FileName can not be null nor empty.");
@@ -117,89 +61,120 @@ public final class FileManager {
     Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
   }
 
-  public File saveSourceCodeFile(final String umlCode, final String targetFilePath) {
-    if (!StringUtils.getInstance().isValid(targetFilePath)) {
-      throw new IllegalArgumentException("Invalid target path.");
+  /**
+   * Deletes a file from the filesystem.
+   * 
+   * @param file2delete
+   *          a <code>File</code> object that represents the file to delete.
+   * @return <code>true</code> if and only if the file is successfully deleted;
+   *         <code>false</code> otherwise
+   * @throws IOException
+   *           if the file does not exist in the file system or if the param
+   *           <code>file2delete</code> represents a directory.
+   * 
+   * @see File
+   */
+  public boolean deleteFile(final File file2delete) throws IOException {
+    if (file2delete == null) {
+      throw new IOException("File to delete can not be null.");
     }
 
-    File targetFile = new File(targetFilePath);
-    return saveSourceCodeFile(umlCode, targetFile);
+    if (!file2delete.exists()) {
+      throw new IOException("File to delete does not exist.");
+    }
+
+    if (file2delete.isDirectory()) {
+      throw new IOException(file2delete.getAbsolutePath() + " is a Folder.");
+    }
+
+    return file2delete.delete();
   }
 
-  public File saveSourceCodeFile(final String umlCode, final File targetFile) {
-    try {
-      if (targetFile == null) {
-        throw new IllegalArgumentException("Invalid target file.");
-      }
-
-      if (!targetFile.exists()) {
-        targetFile.createNewFile();
-      }
-
-      FileWriter fileWriter = new FileWriter(targetFile);
-      BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-
-      bufferedWriter.write(umlCode);
-      bufferedWriter.flush();
-      bufferedWriter.close();
-    } catch (IOException e) {
-      throw new IllegalArgumentException("File could not be saved.");
+  /**
+   * Deletes a file from the filesystem.
+   * 
+   * @param fileAbsolutePath
+   *          the absolute path to the directory to delete.
+   * @return <code>true</code> if and only if the file is successfully deleted;
+   *         <code>false</code> otherwise
+   * @throws IOException
+   *           if the file does not exist in the filesystem or if the param
+   *           <code>fileAbsolutePath</code> represents a directory.
+   */
+  public boolean deleteFile(final String fileAbsolutePath) throws IOException {
+    if (!StringUtils.getInstance().isValid(fileAbsolutePath)) {
+      throw new IllegalArgumentException("Invalid file path");
     }
 
-    return targetFile;
+    File file2delete = new File(fileAbsolutePath);
+    return deleteFile(file2delete);
   }
 
-  public String openSourceCodeFile(final File sourceFile) {
-    StringBuilder builder = new StringBuilder();
-
-    try {
-      if (sourceFile == null) {
-        throw new IllegalArgumentException("Invalid source file.");
-      }
-
-      if (!sourceFile.exists()) {
-        throw new IllegalArgumentException("File " + sourceFile.getAbsolutePath() + " does not exist.");
-      }
-
-      FileReader fileReader = new FileReader(sourceFile);
-      BufferedReader bufferedReader = new BufferedReader(fileReader);
-
-      String currentLine = null;
-
-      while ((currentLine = bufferedReader.readLine()) != null) {
-        builder.append(currentLine);
-        builder.append("\n");
-      }
-
-      bufferedReader.close();
-      fileReader.close();
-    } catch (IOException e) {
-      throw new IllegalArgumentException("Error reading file " + sourceFile.getAbsolutePath() + "\n" + e.getMessage());
+  /**
+   * Deletes a directory and its content from the filesystem.
+   * 
+   * @param folder2Delete
+   *          a <code>File</code> object that represents the directory to
+   *          delete.
+   * @return <code>true</code> if and only if the directory is successfully
+   *         deleted; <code>false</code> otherwise
+   * @throws IOException
+   *           if the directory does not exist in the file system, if the param
+   *           <code>folderAbsolutePath</code> represents a file, or if any
+   *           element from the directory's content can not be deleted.
+   * 
+   * @see File
+   */
+  public boolean deleteFolder(final File folder2Delete) throws IOException {
+    if (folder2Delete == null) {
+      throw new IllegalArgumentException("Folder to delete can not be null.");
     }
 
-    return builder.toString().trim();
+    if (!folder2Delete.exists()) {
+      throw new IOException("Folder to delete does not exist.");
+    }
+
+    if (folder2Delete.isFile()) {
+      throw new IOException(folder2Delete.getAbsolutePath() + " is a File.");
+    }
+
+    if (folder2Delete.listFiles().length == 0) {
+      return folder2Delete.delete();
+    }
+
+    for (File file : folder2Delete.listFiles()) {
+      if (file.isDirectory()) {
+        if (!deleteFolder(file)) {
+          throw new IOException("Impossible to delete folder " + file.getAbsolutePath());
+        }
+      } else if (file.isFile()) {
+        if (!deleteFile(file)) {
+          throw new IOException("Impossible to delete file " + file.getAbsolutePath());
+        }
+      }
+    }
+
+    return folder2Delete.delete();
   }
 
-  public File getParentFolder(final String filePath) {
-    if (!StringUtils.getInstance().isValid(filePath)) {
-      throw new IllegalArgumentException("Invalid file path.");
+  /**
+   * Deletes a directory and its content from the filesystem.
+   * 
+   * @param folderAbsolutePath
+   *          the absolute path to the directory to delete.
+   * @return <code>true</code> if and only if the directory is successfully
+   *         deleted; <code>false</code> otherwise
+   * @throws IOException
+   *           if the directory does not exist in the file system or if the
+   *           param <code>folderAbsolutePath</code> represents a file.
+   */
+  public boolean deleteFolder(final String folderAbsolutePath) throws IOException {
+    if (!StringUtils.getInstance().isValid(folderAbsolutePath)) {
+      throw new IllegalArgumentException("Invalid directory path");
     }
 
-    File file = new File(filePath);
-
-    return getParentFolder(file);
-  }
-
-  public File getParentFolder(final File file) {
-    if (file == null) {
-      throw new IllegalArgumentException("File can not be null");
-    }
-
-    if (!file.exists()) {
-      throw new IllegalArgumentException("File " + file.getAbsolutePath() + " does not exist.");
-    }
-
-    return file.getParentFile();
+    File folder2Delete = new File(folderAbsolutePath);
+    return deleteFolder(folder2Delete);
   }
 
   public String getFileExtension(final File file) {
@@ -226,5 +201,40 @@ public final class FileManager {
     extension = filePath.substring(filePath.lastIndexOf(".") + 1);
 
     return extension;
+  }
+
+  /**
+   * Retrives the only instance of this class.
+   * 
+   * @return an instance of this class.
+   */
+  public static FileManager getInstance() {
+    if (instance == null) {
+      instance = new FileManager();
+    }
+
+    return instance;
+  }
+
+  public File getParentFolder(final File file) {
+    if (file == null) {
+      throw new IllegalArgumentException("File can not be null");
+    }
+
+    if (!file.exists()) {
+      throw new IllegalArgumentException("File " + file.getAbsolutePath() + " does not exist.");
+    }
+
+    return file.getParentFile();
+  }
+
+  public File getParentFolder(final String filePath) {
+    if (!StringUtils.getInstance().isValid(filePath)) {
+      throw new IllegalArgumentException("Invalid file path.");
+    }
+
+    File file = new File(filePath);
+
+    return getParentFolder(file);
   }
 }
