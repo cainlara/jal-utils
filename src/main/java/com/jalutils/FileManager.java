4fd543c0c9baafacb1e8f0a9e3a3b0c9f696b00f
Jose Andrés Lara Vecino <cainlara@users.noreply.github.com>
2019-05-07 12:19:48 -0700

    Adding full javadoc to FileUtils

--- src/main/java/com/jalutils/FileManager.java
+++ src/main/java/com/jalutils/FileManager.java
@@ -21,15 +21,29 @@ public final class FileManager {
   }
 
   /**
+   * Retrives the only instance of this class.
+   * 
+   * @return an instance of this class.
+   */
+  public static FileManager getInstance() {
+    if (instance == null) {
+      instance = new FileManager();
+    }
+
+    return instance;
+  }
+
+  /**
    * Appends the specified extension to the file name.
    * <p>
    * If the file's name does not end with the the extension specified by param
-   * <code>extension</code>, then the extension is appended.
+   * <code>extension</code>, then the extension is appended. <b>NOTE: </b>
+   * invoking this method does not modify the file's name in the local file
+   * system, nor the JVM reference to the file.
    * 
-   * @param fileName
-   *          the current file name.
-   * @param extension
-   *          the extension to append.
+   * @param fileName  the current file name.
+   * @param extension the extension to append.
+   * 
    * @return the fixed file's name ending with the extension specification.
    */
   public String addExtension(final String fileName, final String extension) {
@@ -46,6 +60,19 @@ public final class FileManager {
     return fixedFileName;
   }
 
+  /**
+   * Copies the file in the file system represented by param
+   * <code>sourceFile</code> into the destination specified by param
+   * <code>targetFile</code>.
+   * 
+   * @param sourceFile The file to be copied.
+   * @param targetFile Copy destination.
+   * 
+   * @throws IOException if the file specified by <code>sourceFile</code> is
+   *                     <code>null</code> or does not exist in the file system,
+   *                     or if the file specified by <code>targetFile</code> is
+   *                     <code>null</code>.
+   */
   public void copyFile(final File sourceFile, final File targetFile) throws IOException {
     if (sourceFile == null || !sourceFile.exists()) {
       throw new IOException("Source file (" + sourceFile.getAbsolutePath() + ") does not exist.");
@@ -64,13 +91,12 @@ public final class FileManager {
   /**
    * Deletes a file from the filesystem.
    * 
-   * @param file2delete
-   *          a <code>File</code> object that represents the file to delete.
+   * @param file2delete a <code>File</code> object that represents the file to
+   *                    delete.
    * @return <code>true</code> if and only if the file is successfully deleted;
    *         <code>false</code> otherwise
-   * @throws IOException
-   *           if the file does not exist in the file system or if the param
-   *           <code>file2delete</code> represents a directory.
+   * @throws IOException if the file does not exist in the file system or if the
+   *                     param <code>file2delete</code> represents a directory.
    * 
    * @see File
    */
@@ -93,13 +119,12 @@ public final class FileManager {
   /**
    * Deletes a file from the filesystem.
    * 
-   * @param fileAbsolutePath
-   *          the absolute path to the directory to delete.
+   * @param fileAbsolutePath the absolute path to the directory to delete.
    * @return <code>true</code> if and only if the file is successfully deleted;
    *         <code>false</code> otherwise
-   * @throws IOException
-   *           if the file does not exist in the filesystem or if the param
-   *           <code>fileAbsolutePath</code> represents a directory.
+   * @throws IOException if the file does not exist in the filesystem or if the
+   *                     param <code>fileAbsolutePath</code> represents a
+   *                     directory.
    */
   public boolean deleteFile(final String fileAbsolutePath) throws IOException {
     if (StringUtils.getInstance().isBlank(fileAbsolutePath)) {
@@ -113,15 +138,14 @@ public final class FileManager {
   /**
    * Deletes a directory and its content from the filesystem.
    * 
-   * @param folder2Delete
-   *          a <code>File</code> object that represents the directory to
-   *          delete.
+   * @param folder2Delete a <code>File</code> object that represents the directory
+   *                      to delete.
    * @return <code>true</code> if and only if the directory is successfully
    *         deleted; <code>false</code> otherwise
-   * @throws IOException
-   *           if the directory does not exist in the file system, if the param
-   *           <code>folderAbsolutePath</code> represents a file, or if any
-   *           element from the directory's content can not be deleted.
+   * @throws IOException if the directory does not exist in the file system, if
+   *                     the param <code>folderAbsolutePath</code> represents a
+   *                     file, or if any element from the directory's content can
+   *                     not be deleted.
    * 
    * @see File
    */
@@ -160,13 +184,12 @@ public final class FileManager {
   /**
    * Deletes a directory and its content from the filesystem.
    * 
-   * @param folderAbsolutePath
-   *          the absolute path to the directory to delete.
+   * @param folderAbsolutePath the absolute path to the directory to delete.
    * @return <code>true</code> if and only if the directory is successfully
    *         deleted; <code>false</code> otherwise
-   * @throws IOException
-   *           if the directory does not exist in the file system or if the
-   *           param <code>folderAbsolutePath</code> represents a file.
+   * @throws IOException if the directory does not exist in the file system or if
+   *                     the param <code>folderAbsolutePath</code> represents a
+   *                     file.
    */
   public boolean deleteFolder(final String folderAbsolutePath) throws IOException {
     if (StringUtils.getInstance().isBlank(folderAbsolutePath)) {
@@ -177,6 +200,18 @@ public final class FileManager {
     return deleteFolder(folder2Delete);
   }
 
+  /**
+   * Retrieves the extension of the file represented by param <code>file</code>.
+   * <p>
+   * The file name must follow the pattern <i>file_name.file_extension</i>,
+   * allowing this method to retrieve the substring starting at the last index of
+   * the dot character.
+   * 
+   * @param file a <code>java.io.File</code> instance to retrieve the extension
+   *             from.
+   * 
+   * @return The file extension.
+   */
   public String getFileExtension(final File file) {
     if (file == null) {
       throw new IllegalArgumentException("File can not be null");
@@ -187,6 +222,18 @@ public final class FileManager {
     return getFileExtension(filePath);
   }
 
+  /**
+   * Retrieves the extension of a file located at <code>filePath</code>.
+   * <p>
+   * The file name must follow the pattern <i>file_name.file_extension</i>,
+   * allowing this method to retrieve the substring starting at the last index of
+   * the dot character.
+   * 
+   * @param filePath a valid <code>String</code> path to the file to retrieve the
+   *                 extension from.
+   * 
+   * @return The file extension.
+   */
   public String getFileExtension(final String filePath) {
     String extension = null;
 
@@ -204,18 +251,14 @@ public final class FileManager {
   }
 
   /**
-   * Retrives the only instance of this class.
+   * Retrieves the container folder of a file.
    * 
-   * @return an instance of this class.
+   * @param file the <code>java.io.File</code>instance to retrieve the container
+   *             folder from.
+   * 
+   * @return a <code>java.io.File</code> instance representing a
+   *         <code>directory</code> which contains the input file.
    */
-  public static FileManager getInstance() {
-    if (instance == null) {
-      instance = new FileManager();
-    }
-
-    return instance;
-  }
-
   public File getParentFolder(final File file) {
     if (file == null) {
       throw new IllegalArgumentException("File can not be null");
@@ -228,6 +271,15 @@ public final class FileManager {
     return file.getParentFile();
   }
 
+  /**
+   * Retrieves the container folder of a file.
+   * 
+   * @param filePath a valid <code>String</code> path to the file to retrieve the
+   *                 container folder from.
+   * 
+   * @return a <code>java.io.File</code> instance representing a
+   *         <code>directory</code> which contains the input file.
+   */
   public File getParentFolder(final String filePath) {
     if (StringUtils.getInstance().isBlank(filePath)) {
       throw new IllegalArgumentException("Invalid file path.");
