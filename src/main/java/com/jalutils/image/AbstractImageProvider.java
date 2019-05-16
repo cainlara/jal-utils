5f8b5d7284025be9becd6d02c70d64c1c2d7fc1d
Jose Andrés Lara Vecino <cainlara@users.noreply.github.com>
2019-05-16 14:27:37 -0700

    Adding image provider class

---
+++ src/main/java/com/jalutils/image/AbstractImageProvider.java
@@ -0,0 +1,23 @@
+package com.jalutils.image;
+
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
+public abstract class AbstractImageProvider {
+
+  /**
+   * @see ImageManager.Builder#build()
+   * 
+   * @return a ImageManager instance
+   */
+  protected abstract ImageManager getImageManager();
+
+  public Image getImage(final String key) {
+    return getImageManager().getImage(key);
+  }
+
+  public ImageIcon getImageIcon(final String key) {
+    return getImageManager().getImageIcon(key);
+  }
+}
